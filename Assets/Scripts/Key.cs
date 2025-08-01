22b8e76333400ab0990b54dfc83fd0578e1e734d
mjjm4d <threehalves1@gmail.com>
2025-07-31 23:37:40 -0500

    Target visual changes

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -6,6 +6,7 @@ public class Key : MonoBehaviour
     [SerializeField] private ParticleSystem _destroyParticles;
     [SerializeField] private bool isShootable;
     [SerializeField] private bool isPlayerCollidable;
+    [SerializeField] private bool destroysProjectileOnCollide;
     [SerializeField] GameObject[] objectsDestroyOnCollect;
 
     public bool collected;
@@ -16,7 +17,7 @@ public class Key : MonoBehaviour
             (other.gameObject.GetComponent<Player2D>() != null || isPlayerCollidable))
         {
             foreach (GameObject obj in objectsDestroyOnCollect) Destroy(obj); // destroy locked doors on collect
-
+            if (other.gameObject.GetComponent<PlayerProjectile>() != null) Destroy(other.gameObject);
             if (_destroyParticles) _destroyParticles.gameObject.transform.parent = null;
             Destroy(gameObject);
             if (_destroyParticles != null) _destroyParticles.Play();
