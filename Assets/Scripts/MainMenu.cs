ae98765d5826cd484cb9e40322dd6ed72c092ca2
mjjm4d <threehalves1@gmail.com>
2025-08-01 17:20:36 -0500

    Title and end screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayPressed()
     {
         GameManager.Instance.StartGame();
+    }
+
+    public void SetScene(string s)
+    {
+        SceneManager.LoadScene(s);
     }
 }
