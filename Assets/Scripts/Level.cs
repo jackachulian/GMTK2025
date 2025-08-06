1af6417e3f7309b900f88ad8d6a045a08b4e9ac4
mjjm4d <threehalves1@gmail.com>
2025-08-05 20:43:50 -0500

    Level hue shifting

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,17 +1,28 @@
-using System;
-using UnityEngine; 
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Level : MonoBehaviour
 {
-    private Player2D _player;
-
     public Vector2 levelSize = new Vector2(20f, 11.25f);
 
     private void Start()
-    {
-        _player = GameObject.FindFirstObjectByType<Player2D>();
-        
+    {        
         GameManager.currentLevel = this;
         AudioManager.Instance.PlayMusic("BGM1");
+
+        // Get color adjustments on property
+        ColorAdjustments colorAdjustments;
+        GameObject.Find("Screen Effects").GetComponent<Volume>().profile.TryGet(out colorAdjustments);
+
+        // make new hue parameter
+        VolumeParameter<float> hue = new()
+        {
+            value = Random.Range(-4, 4) * 45f
+        };
+
+        // assign parameter
+        if (colorAdjustments != null)
+            colorAdjustments.hueShift.SetValue(hue);
     }
 }
